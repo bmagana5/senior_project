3a3cc7bf9a448cfa84e0667a3a0ed0960b7b4b5f
Brian Magana <bmagana5@yahoo.com>
2020-11-28 02:43:47 -0800

    Changed form input field data validation. It is now handled by Javascript instead of PHP. Added a simple footer script and a .gitignore file.

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <?php
-    echo "<p class='container' style='font-size: 12px;'>SpamBook &copy; " . date('Y');
+    echo "<p class='container' style='font-size: 12px;'><img src='img/spam.png' alt='picture of spam' width='100' height='100'>SpamBook &copy; " . date('Y');
 ?>
--- js/signup.js
+++ js/signup.js
@@ -1,25 +1,102 @@
-function verifyUsername(username, e) {
-    // this informs the user that usernames must be at least 6 characters long
-    if (username.length < 6 && username.length != 0) {
-        $('#usernameField').tooltip('show');
-    } else {
-        $('#usernameField').tooltip('hide');
+// this will keep track of the validity of form fields
+let isFieldValid = {username:false, fullname:false, email:false, password:false, confirmpassword:false};
+
+// this function will be called as the user types in <input> value fields in the sign up form
+function validateField(field) {
+    switch (field.id) {
+        case 'usernameField':
+            let usernameCriteria = {length:false, input:false};
+            if (field.value.length < 6 && field.value.length != 0) {
+                $('#'+field.id).tooltip('show');
+                usernameCriteria['length'] = false;
+            } else if (field.value.length == 0) {
+                usernameCriteria['length'] = false;
+            } else {
+                $('#'+field.id).tooltip('hide');
+                usernameCriteria['length'] = true;
+            }
+            if (field.value.match(/^[0-9a-zA-Z_\b\.]+$/) == null && field.value.length != 0) {
+                document.getElementById('error'+field.id).innerHTML = 'Invalid symbol detected';
+                usernameCriteria['input'] = false;
+            } else {
+                document.getElementById('error'+field.id).innerHTML = '';
+                usernameCriteria['input'] = true;
+            }
+            if (usernameCriteria['length'] == true && usernameCriteria['input'] == true) {
+                isFieldValid['username'] = true;
+            } else {
+                isFieldValid['username'] = false;
+            }
+            break;
+        case 'fullnameField':
+            if (field.value.match(/^[a-zA-Z \-'\b]+$/) == null && field.value.length != 0) {
+                document.getElementById('error'+field.id).innerHTML = 'Invalid symbol detected';
+                isFieldValid['fullname'] = false;
+            } else {
+                document.getElementById('error'+field.id).innerHTML = '';
+                isFieldValid['fullname'] = true;
+            }
+            break;
+        case 'emailField':
+            if (field.value.match(/^[a-zA-Z0-9!#$%&'*+-\/=?^_`{|}~\b]+[@][a-zA-z\b]+[\.][a-zA-Z\b]+$/) == null && field.value.length != 0) {
+                isFieldValid['email'] = false;
+                if (field.value.match(/@{1}/) == null) {
+                    document.getElementById('error'+field.id).innerHTML = "Needs '@' symbol. ";
+                } else {
+                    document.getElementById('error'+field.id).innerHTML = '';
+                }
+                if (field.value.match(/\.{1}/) == null) {
+                    document.getElementById('error'+field.id).innerHTML += "Needs '.' symbol. ";
+                } else {
+                    document.getElementById('error'+field.id).innerHTML = '';
+                }
+                document.getElementById('error'+field.id).innerHTML += 'Invalid email.';
+            } else {
+                document.getElementById('error'+field.id).innerHTML = '';
+                isFieldValid['email'] = true;
+            }
+            break;
+        case 'passwordField':
+            if (field.value.length < 8 && field.value.length != 0) {
+                $('#'+field.id).tooltip('show');
+                isFieldValid['password'] = false;
+            } else {
+                $('#'+field.id).tooltip('hide');
+                isFieldValid['password'] = true;
+            }
+            break;
+        default:
+            alert("shit's not working");
+            break;
     }
 }
 
-function verifyPassword(password, e) {
-    if (password.length < 8 && password.length != 0) {
-        $('#passwordField').tooltip('show');
+// this function will ensure that users are aware of which characters are put into password form fields.
+function checkPassword(password, confirm) {
+    if (confirm !== password) {
+        $('#confirmpasswordField').tooltip('show');
+        isFieldValid['confirmpassword'] = false;
     } else {
-        $('#passwordField').tooltip('hide');
+        $('#confirmpasswordField').tooltip('hide');
+        isFieldValid['confirmpassword'] = true;
     }
 }
 
-
-
-function filterKey(e) {
-    // we can filter out unwanted characters from even entering input fields here 
-    let key = e.which || e.keyCode;
-    key = String.fromCharCode(key);
-    // alert(key);
+// this function will ensure that when the form is submitted, it'll only succeed if the 
+// form data is completely valid
+function confirmFieldValidity() {
+    if (isFieldValid['username'] == true && isFieldValid['fullname'] == true && 
+        isFieldValid['email'] == true && isFieldValid['password'] == true && 
+        isFieldValid['confirmpassword'] == true) {
+        // alert('be free');
+        return true;
+    } else {
+        return false;
+        // alert(`you shall not pass: username: ${isFieldValid['username']}
+        //     fullname: ${isFieldValid['fullname']}
+        //     email: ${isFieldValid['email']}
+        //     password: ${isFieldValid['password']}
+        //     confirmpassword: ${isFieldValid['confirmpassword']}` 
+        // );
+    }
 }
